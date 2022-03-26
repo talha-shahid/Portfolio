eade89e97985f518721a56b9e45b45f4deb93a37
Talha Shahid <talhashahidch@gmail.com>
2022-03-26 15:30:03 +0500

    Added dancing figures and disco settings

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 console.log("Welcome");
+let pepe=document.getElementById("pepe");
+let pepe2=document.getElementById("pepe2");
+let peter=document.getElementById("peter");
+let infoContainer= document.getElementById("infoContainer");
 
+pepe.style.display="none";
+pepe2.style.display="none";
+peter.style.display="none";
 document.querySelector('.cross').style.display = 'none';
 document.querySelector('.icons').addEventListener('click', ()=>{
     document.querySelector('.sidebar').classList.toggle("sidebarGo");
@@ -20,17 +27,48 @@ document.querySelector('.icons').addEventListener('click', ()=>{
 audio=new Audio("parasthesia.mp3");
 let sound= document.getElementById("sound");
 
+// pepe.style.transition = "all 2s";
 sound.addEventListener("click",()=>{
     if (audio.duration > 0 && !audio.paused) {
 
         //Its playing...do your job
         audio.pause();
         sound.src="nosound.png";
+        // infoContainer.classList.remove("mystyle");
+        
+        setTimeout(() => {
+            pepe.style.display="none";
+            pepe2.style.display="none";
+            peter.style.display="none";
+
+        }, 100);
+
     
     } else {
     
         //Not playing...maybe paused, stopped or never played.
-        audio.play();
+        audio.loop=true;
+            audio.play();
+
+
         sound.src="sound.png";
+        setTimeout(() => {
+
+
+            const myInterval = setInterval(() => {
+                infoContainer.classList.add("mystyle");
+                setTimeout(() => {
+                    infoContainer.classList.remove("mystyle");
+                }, 300);
+            }, 600);
+
+            sound.addEventListener("click", ()=>{clearInterval(myInterval);})
+
+            pepe.style.display="inline";
+            pepe2.style.display="inline";
+            peter.style.display="inline";
+        }, 100);
     }
 })
+
+
